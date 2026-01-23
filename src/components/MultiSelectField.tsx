1f6a60e715484d253d9f4a1e720b45a73d37b28c
leonhenrik <106387041+leonhenrik@users.noreply.github.com>
2026-01-23 10:27:22 -0500

    dropdwon opening fixed

--- src/components/MultiSelectField.tsx
+++ src/components/MultiSelectField.tsx
@@ -159,7 +159,7 @@ export function MultiSelectField({
             )}
           </Button>
         </PopoverTrigger>
-        <PopoverContent className="w-full p-0" align="start">
+        <PopoverContent className="w-full p-0" align="start" side="bottom">
           <Command>
             {searchable && (
               <div className="flex items-center border-b px-3">
